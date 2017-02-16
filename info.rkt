60801771a6a29f44846bb7e7da98f061af2825a9
Mark Wunsch <mark@markwunsch.com>
2017-02-16 09:10:47 -0500

    raco pkg new

---
+++ info.rkt
@@ -0,0 +1,9 @@
+#lang info
+(define collection "chyron")
+(define deps '("base"
+               "rackunit-lib"))
+(define build-deps '("scribble-lib" "racket-doc"))
+(define scribblings '(("scribblings/chyron.scrbl" ())))
+(define pkg-desc "Description Here")
+(define version "0.0")
+(define pkg-authors '(markwunsch))
---
+++ scribblings/chyron.scrbl
@@ -0,0 +1,10 @@
+#lang scribble/manual
+@require[@for-label[chyron
+                    racket/base]]
+
+@title{chyron}
+@author{markwunsch}
+
+@defmodule[chyron]
+
+Package Description Here
