83e954c5ad250dade7d5a5be5982580b7384be29
Mark Wunsch <mark@markwunsch.com>
2017-03-06 17:43:51 -0500

    get metadata from gir

--- ffi/introspection.rkt
+++ ffi/introspection.rkt
@@ -1,8 +1,16 @@
 #lang racket/base
 
 (require ffi/unsafe
-         ffi/unsafe/define)
+         ffi/unsafe/define
+         ffi/unsafe/alloc)
 
-(define-ffi-definer define-g-irepository (ffi-lib "libgirepository-1.0"))
+(define-ffi-definer define-gir (ffi-lib "libgirepository-1.0"))
 
-(define-g-irepository g_irepository_require (_fun (_pointer = #f) _string _string _int _pointer -> _pointer))
+(define _GITypelib (_cpointer 'GITypelib))
+
+(define-gir g_irepository_require (_fun (_pointer = #f) _string _string _int _pointer -> _GITypelib))
+(define-gir g_irepository_find_by_name (_fun (_pointer = #f) _string _string -> _pointer))
+(define-gir g_irepository_get_n_infos (_fun (_pointer = #f) _string -> _int))
+
+(define (gir-require namespace)
+  (g_irepository_require namespace #f 0 #f))
