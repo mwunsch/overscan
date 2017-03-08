e8776c92d9ed42ec752951b2007ff88fa0a4dac8
Mark Wunsch <mark@markwunsch.com>
2017-03-08 11:53:45 -0500

    Convert GError pointers into 1st class values

--- ffi/introspection.rkt
+++ ffi/introspection.rkt
@@ -6,7 +6,9 @@
 
 (define-ffi-definer define-gir (ffi-lib "libgirepository-1.0"))
 
-(define _gi-base-info (_cpointer 'GIBaseInfo))
+(define-cstruct _gerror ([domain _uint32] [code _int] [message _string]))
+(define _gi-type-lib (_cpointer/null 'GITypelib))
+(define _gi-base-info (_cpointer/null 'GIBaseInfo))
 (define _gi-info-type (_enum '(invalid
                                function
                                callback
@@ -34,15 +36,20 @@
 (define-gir g_base_info_unref (_fun _gi-base-info -> _void)
   #:wrap (deallocator))
 
-(define-gir g_irepository_require (_fun (_pointer = #f) _string _string _int _pointer -> _pointer))
+(define-gir g_irepository_require (_fun (_pointer = #f) _string _string _int (err : (_ptr io _gerror-pointer/null) = #f)
+                                        -> (r : _gi-type-lib)
+                                        -> (or r
+                                               (error (gerror-message err)))))
 (define-gir g_irepository_get_n_infos (_fun (_pointer = #f) _string -> _int))
 (define-gir g_irepository_get_info (_fun (_pointer = #f) _string _int -> _gi-base-info)
   #:wrap (allocator g_base_info_unref))
 (define-gir g_irepository_find_by_name (_fun (_pointer = #f) _string _string -> _gi-base-info)
   #:wrap (allocator g_base_info_unref))
 
+(define _gi-function-info (_cpointer 'GIFunctionInfo))
+
 (define (gir-require namespace)
-  (g_irepository_require namespace #f 0 #f))
+  (g_irepository_require namespace #f 0))
 
 (define (introspect namespace)
   (gir-require namespace)
