7e1a037439add2a1b453aae67808c09bc830f8eb
Mark Wunsch <mark@markwunsch.com>
2017-04-03 11:00:12 -0400

    use _symbol ctype for working with introspected names

--- ffi/introspection.rkt
+++ ffi/introspection.rkt
@@ -77,9 +77,9 @@
 
 
 ;;; BaseInfo
-(define-gir g_base_info_get_namespace (_fun _gi-base-info -> _string))
+(define-gir g_base_info_get_namespace (_fun _gi-base-info -> _symbol))
 
-(define-gir g_base_info_get_name (_fun _gi-base-info -> _string))
+(define-gir g_base_info_get_name (_fun _gi-base-info -> _symbol))
 
 (define-gir g_base_info_get_type (_fun _gi-base-info -> _gi-info-type))
 
@@ -96,17 +96,17 @@
   (g_base_info_get_name base))
 
 ;;; Repositories
-(define-gir g_irepository_require (_fun (_pointer = #f) _string _string _int (err : (_ptr io _gerror-pointer/null) = #f)
+(define-gir g_irepository_require (_fun (_pointer = #f) _symbol _string _int (err : (_ptr io _gerror-pointer/null) = #f)
                                         -> (r : _pointer)
                                         -> (or r
                                                (error (gerror-message err)))))
 
-(define-gir g_irepository_get_n_infos (_fun (_pointer = #f) _string -> _int))
+(define-gir g_irepository_get_n_infos (_fun (_pointer = #f) _symbol -> _int))
 
-(define-gir g_irepository_get_info (_fun (_pointer = #f) _string _int -> _gi-base-info)
+(define-gir g_irepository_get_info (_fun (_pointer = #f) _symbol _int -> _gi-base-info)
   #:wrap (allocator g_base_info_unref))
 
-(define-gir g_irepository_find_by_name (_fun (_pointer = #f) _string _string -> _gi-base-info)
+(define-gir g_irepository_find_by_name (_fun (_pointer = #f) _symbol _symbol -> _gi-base-info)
   #:wrap (allocator g_base_info_unref))
 
 
@@ -347,7 +347,7 @@
 (define-gir g_struct_info_get_method (_fun _gi-base-info _int -> _gi-base-info)
   #:wrap (allocator g_base_info_unref))
 
-(define-gir g_struct_info_find_method (_fun _gi-base-info _string -> _gi-base-info)
+(define-gir g_struct_info_find_method (_fun _gi-base-info _symbol -> _gi-base-info)
   #:wrap (allocator g_base_info_unref))
 
 (struct gi-struct (info)
@@ -411,17 +411,16 @@
     (let ([_info (g_irepository_get_info namespace i)])
       (cons (g_base_info_get_type _info) (g_base_info_get_name _info)))))
 
-(define (introspection namesym)
-  (let ([namespace (symbol->string namesym)])
-    (g_irepository_require namespace #f 0)
-    (lambda (name)
-      (let ([info (or (g_irepository_find_by_name namespace (symbol->string name))
-                      (raise-argument-error 'introspection "name in GIR namespace" name))])
-        (gi-binding info)))))
+(define (introspection namespace)
+  (g_irepository_require namespace #f 0)
+  (lambda (name)
+    (let ([info (or (g_irepository_find_by_name namespace name)
+                    (raise-argument-error 'introspection "name in GIR namespace" name))])
+      (gi-binding info))))
 
 (define (gi-binding info)
   (let ([info-type (g_base_info_get_type info)]
-        [info-name (string->symbol (g_base_info_get_name info))])
+        [info-name (g_base_info_get_name info)])
     (case info-type
       ['GI_INFO_TYPE_FUNCTION (gi-function info)]
       ['GI_INFO_TYPE_STRUCT (gi-struct info)]
