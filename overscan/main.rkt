221476be35f29dcfc8511fe51e505edf1125872e
Mark Wunsch <mark@markwunsch.com>
2017-05-17 11:37:04 -0400

    Encode video and audio, mux to an flv and record out to filesystem

--- overscan/main.rkt
+++ overscan/main.rkt
@@ -11,28 +11,74 @@
 
 (define current-broadcast (box #f))
 
+(define video-preview (element-factory% 'make "osxvideosink" "sink:preview"))
+
+(define false-preview (element-factory% 'make "fakesink" "sink:fakepreview"))
+
+(define filter-video (caps% 'from_string "video/x-raw"))
+
+(define h264-encoder (element-factory% 'make "vtenc_h264" "encode:h264"))
+
+(define aac-encoder (element-factory% 'make "faac" "encode:aac"))
+
+(define flvmuxer
+  (let ([muxer (element-factory% 'make "flvmux" "mux:flv")])
+    (gobject-set! muxer "streamable" #t _bool)
+    muxer))
+
+(define false-recording (element-factory% 'make "fakesink" "sink:fakerecording"))
+
 (define (broadcast scene
-                   #:preview [preview (element-factory% 'make "osxvideosink" #f)]
-                   #:record [recording ()])
-  (let ([pipeline (pipeline% 'new "broadcast")])
-    (and (bin-add-many pipeline scene preview)
-         (element-link-many scene preview)
-         (send pipeline set-state 'playing)
-         (set-box! current-broadcast pipeline))))
+                   #:preview [preview video-preview]
+                   #:record [record #f])
+  (let ([pipeline (pipeline% 'new "broadcast")]
+        [tee (element-factory% 'make "tee" #f)]
+        [queue (element-factory% 'make "queue" #f)]
+        [record-sink (if record
+                         (recording record)
+                         false-recording)]
+        [preview (or preview
+                     false-preview)])
+    (or (and (bin-add-many pipeline scene tee queue preview h264-encoder aac-encoder flvmuxer record-sink)
+             (send scene link-filtered tee filter-video)
+             (element-link-many tee queue:preview preview)
+             (element-link-many tee h264-encoder flvmuxer)
+             (element-link-many scene aac-encoder flvmuxer)
+             (send flvmuxer link record-sink)
+             (send pipeline set-state 'playing)
+             (set-box! current-broadcast pipeline))
+        (error "Couldn't start broadcast"))))
 
 (define (end-broadcast [broadcast (unbox current-broadcast)])
-  (and broadcast
+  (and (or broadcast
+           (error "there is no current broadcast"))
        (send broadcast send-event (event% 'new_eos))
        (send broadcast set-state 'null)
        (set-box! current-broadcast #f)))
 
-(define (scene)
+(define (scene . sources)
   (let ([bin (bin% 'new "scene")]
-        [camera (element-factory% 'make "avfvideosrc" "camera")])
-    (if (send bin add camera)
-        (let* ([pad (send camera get-static-pad "src")])
-          (or (and pad
-                   (send bin add-pad ((gst 'GhostPad) 'new "src" pad))
+        [camera (element-factory% 'make "avfvideosrc" "camera")]
+        [audio (element-factory% 'make "osxaudiosrc" "microphone")])
+    (if (bin-add-many bin camera audio)
+        (let* ([camera-pad (send camera get-static-pad "src")]
+               [audio-pad (send audio get-static-pad "src")])
+          (or (and camera-pad
+                   (send bin add-pad ((gst 'GhostPad) 'new "video" camera-pad))
+                   audio-pad
+                   (send bin add-pad ((gst 'GhostPad) 'new "audio" audio-pad))
                    bin)
               (error "could not create scene")))
         (error "could not create scene"))))
+
+(define (recording location)
+  (let ([bin (bin% 'new "recording")]
+        [filesink (element-factory% 'make "filesink" #f)])
+    (gobject-set! filesink "location" location _path)
+    (or (and (send bin add filesink)
+             (let ([pad (send filesink get-static-pad "sink")])
+               (or (and pad
+                        (send bin add-pad ((gst 'GhostPad) 'new "sink" pad))
+                        bin)
+                   (error "could not get sink-pad for recording"))))
+        (error "could not make a recording"))))
