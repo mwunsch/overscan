688ab2f5f09178169b1d7fe99b9926662368f748
Mark Wunsch <mark@markwunsch.com>
2017-09-07 17:33:41 -0400

    Determine if an element is a src? or a sink?

--- gstreamer/element.rkt
+++ gstreamer/element.rkt
@@ -9,3 +9,10 @@
 (define element-factory% (gst 'ElementFactory))
 
 (define element% (gst 'Element))
+
+(define (sink? el)
+  (and (zero? (get-field numsrcpads el))
+       (positive? (get-field numsinkpads el))))
+
+(define (src? el)
+  (not (sink? el)))
