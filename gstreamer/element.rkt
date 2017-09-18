f0be0a1f67859f56d0677290efbc92d8413b9b7b
Mark Wunsch <mark@markwunsch.com>
2017-09-18 17:38:32 -0400

    Write element factory and element as racket/class

--- gstreamer/element.rkt
+++ gstreamer/element.rkt
@@ -1,14 +1,26 @@
 #lang racket/base
 
-(require ffi/unsafe/introspection
+(require (except-in ffi/unsafe/introspection
+                    send get-field set-field! field-bound? is-a? is-a?/c)
+         racket/class
          "gst.rkt")
 
 (provide element%
          element-factory%)
 
-(define element-factory% (gst 'ElementFactory))
+(define element-factory%
+  (let ([elfac (gst 'ElementFactory)])
+    (class* object% (gobject<%>)
+            (init-field name)
+            (field [pointer (elfac 'find name)])
+            (super-new)
+            (define/public (create name)
+              (let ([el (gobject-send pointer 'create name)])
+                (new element% [pointer el]))))))
 
-(define element% (gst 'Element))
+(define element%
+  (class gobject%
+    (super-new)))
 
 (define (sink? el)
   (and (zero? (get-field numsrcpads el))
