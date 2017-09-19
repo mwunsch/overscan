4337bceb4fe3cb89596a955b079f725e84e68d1c
Mark Wunsch <mark@markwunsch.com>
2017-09-19 12:32:09 -0400

    Define methods on element and element-factory

--- gstreamer/element.rkt
+++ gstreamer/element.rkt
@@ -5,22 +5,42 @@
          racket/class
          "gst.rkt")
 
-(provide element%
-         element-factory%)
+(provide element-factory%
+         element%
+         element-factory%-find
+         element-factory%-make)
+
+(define gst-element-factory (gst 'ElementFactory))
 
 (define element-factory%
-  (let ([elfac (gst 'ElementFactory)])
-    (class* object% (gobject<%>)
-            (init-field name)
-            (field [pointer (elfac 'find name)])
-            (super-new)
-            (define/public (create name)
-              (let ([el (gobject-send pointer 'create name)])
-                (new element% [pointer el]))))))
+  (class gobject%
+         (super-new)
+         (inherit-field pointer)
+         (define/public (get-name)
+           (gobject-send pointer 'get_name))
+         (define/public (create name)
+           (let ([el (gobject-send pointer 'create name)])
+             (new element% [pointer el])))
+         (define/public (get-metadata)
+           (for/hash ([key (gobject-send pointer 'get_metadata_keys)])
+                     (values (string->symbol key)
+                             (gobject-send pointer 'get_metadata key))))))
+
+(define (element-factory%-find name)
+  (let ([factory (gst-element-factory 'find name)])
+    (new element-factory% [pointer factory])))
+
+(define (element-factory%-make factory-name name)
+  (let ([el (gst-element-factory 'make factory-name name)])
+    (new element% [pointer el])))
 
 (define element%
   (class gobject%
-    (super-new)))
+    (super-new)
+    (inherit-field pointer)
+    (define/public (get-factory)
+      (let ([fac (gobject-send pointer 'get_factory)])
+        (new element-factory% [pointer fac])))))
 
 (define (sink? el)
   (and (zero? (get-field numsrcpads el))
