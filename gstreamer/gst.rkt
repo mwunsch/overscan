c96c04aee8e5c83122bc9e74b7641365d52b83bb
Mark Wunsch <mark@markwunsch.com>
2017-09-25 11:28:08 -0400

    Create a gst-object% base class for other gst objects

--- gstreamer/gst.rkt
+++ gstreamer/gst.rkt
@@ -1,7 +1,37 @@
 #lang racket/base
 
-(require ffi/unsafe/introspection)
+(require (except-in ffi/unsafe/introspection
+                    send get-field set-field! field-bound? is-a? is-a?/c)
+         racket/class
+         racket/contract)
 
-(provide gst)
+(provide (contract-out [gst
+                        gi-repository?]
+                       [gst-object%
+                        (class/c
+                         [get-name
+                          (->m string?)]
+                         [get-parent
+                          (->m (or/c gobject? #f))]
+                         [has-as-parent?
+                          (->m gobject? boolean?)]
+                         [get-path-string
+                          (->m string?)])]
+                       [gst-version
+                        (-> string?)]))
 
 (define gst (introspection 'Gst))
+
+(define gst-object-mixin
+  (make-gobject-delegate get-name
+                         get-parent
+                         [has-as-parent? 'has_as_parent]
+                         get-path-string))
+
+(define gst-object%
+  (class (gst-object-mixin gobject%)
+    (super-new)
+    (inherit-field pointer)))
+
+(define gst-version
+  (gst 'version_string))
