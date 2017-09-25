f3350fff9f8218b45aaa50327a693493d7d74002
Mark Wunsch <mark@markwunsch.com>
2017-09-25 11:29:00 -0400

    element% and element-factory% inherit from gst-object%

--- gstreamer/element.rkt
+++ gstreamer/element.rkt
@@ -3,22 +3,27 @@
 (require (except-in ffi/unsafe/introspection
                     send get-field set-field! field-bound? is-a? is-a?/c)
          racket/class
+         racket/contract
          "gst.rkt")
 
-(provide element-factory%
-         element%
-         element-factory%-find
-         element-factory%-make)
+(provide (contract-out [element-factory%
+                        (subclass?/c gst-object%)]
+                       [element%
+                        (subclass?/c gst-object%)]
+                       [element-factory%-find
+                        (-> string? (is-a?/c element-factory%))]
+                       [element-factory%-make
+                        (->* (string?)
+                             ((or/c string? false/c))
+                             (is-a?/c element%))]))
 
 (define gst-element-factory (gst 'ElementFactory))
 
 (define element-factory%
-  (class gobject%
+  (class gst-object%
          (super-new)
          (inherit-field pointer)
-         (define/public (get-name)
-           (gobject-send pointer 'get_name))
-         (define/public (create name)
+         (define/public (create [name #f])
            (let ([el (gobject-send pointer 'create name)])
              (new element% [pointer el])))
          (define/public (get-metadata)
@@ -30,20 +35,19 @@
   (let ([factory (gst-element-factory 'find name)])
     (new element-factory% [pointer factory])))
 
-(define (element-factory%-make factory-name name)
+(define (element-factory%-make factory-name [name #f])
   (let ([el (gst-element-factory 'make factory-name name)])
     (new element% [pointer el])))
 
 (define element-mixin
-  (make-gobject-delegate get-name get-factory))
+  (make-gobject-delegate get-factory))
 
 (define element%
-  (class (element-mixin gobject%)
+  (class (element-mixin gst-object%)
     (super-new)
     (inherit-field pointer)
     (define/override (get-factory)
-      (let ([fac (super get-factory)])
-        (new element-factory% [pointer fac])))
+      (new element-factory% [pointer (super get-factory)]))
     (define/public (get-num-src-pads)
       (gobject-get-field 'numsrcpads pointer))
     (define/public (get-num-sink-pads)
