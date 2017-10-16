27a47300f589586fb70182cd244a0a097d4c56d8
Mark Wunsch <mark@markwunsch.com>
2017-10-16 16:55:35 -0400

    Add rtmpsink as a common element and move twitch.rkt to use it

--- gstreamer/elements.rkt
+++ gstreamer/elements.rkt
@@ -11,8 +11,16 @@
 (provide (contract-out [capsfilter
                         (->* (caps?)
                              ((or/c string? false/c))
-                             (is-a?/c element%))]))
+                             (is-a?/c element%))]
+                       [rtmpsink
+                        (->* (string?)
+                             ((or/c string? false/c))
+                             sink?)]))
 
 (define (capsfilter caps [name #f])
   (gobject-with-properties (element-factory%-make "capsfilter" name)
                            (hash 'caps caps)))
+
+(define (rtmpsink location [name #f])
+  (gobject-with-properties (element-factory%-make "rtmpsink" name)
+                           (hash 'location location)))
--- overscan/twitch.rkt
+++ overscan/twitch.rkt
@@ -1,23 +1,21 @@
 #lang racket/base
 
-(require gstreamer
-         ffi/unsafe/introspection
-         racket/contract)
+(require racket/contract
+         gstreamer)
 
 (provide (contract-out [twitch-sink
-                        (->* () (#:test boolean?) (is-a?/c element%))]
+                        (->* () (#:test boolean?) sink?)]
                        [twitch-stream-key
                         (parameter/c string?)]))
 
 (define twitch-stream-key (make-parameter (getenv "TWITCH_STREAM_KEY")))
 
 (define (twitch-sink #:test [bandwidth-test #f])
-  (let* ([stream-key (twitch-stream-key)]
-         [rtmp (element-factory% 'make "rtmpsink" "sink:rtmp:twitch")]
-         [location (format "rtmp://live-jfk.twitch.tv/app/~a~a live=1"
-                           stream-key
-                           (if bandwidth-test "?bandwidthtest=true" ""))])
-    (unless stream-key
-      (error "no TWITCH_STREAM_KEY in env"))
-    (gobject-set! rtmp "location" location)
-    rtmp))
+  (define stream-key (twitch-stream-key))
+  (unless stream-key
+    (error "no TWITCH_STREAM_KEY in env"))
+
+  (let ([location (format "rtmp://live-jfk.twitch.tv/app/~a~a live=1"
+                          stream-key
+                          (if bandwidth-test "?bandwidthtest=true" ""))])
+    (rtmpsink location "sink:rtmp:twitch")))
