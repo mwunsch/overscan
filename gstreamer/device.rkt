e90e2cb2995d30da8d94808defb41b6bba5efe76
Mark Wunsch <mark@markwunsch.com>
2017-11-15 18:05:04 -0500

    contracts for device and devicemonitor

--- gstreamer/device.rkt
+++ gstreamer/device.rkt
@@ -3,21 +3,23 @@
 (require ffi/unsafe/introspection
          racket/class
          racket/contract
-         gstreamer/gst)
+         gstreamer/gst
+         gstreamer/caps
+         gstreamer/element
+         gstreamer/bus)
 
 (provide (contract-out [device-monitor%
                         (class/c
-                         get-bus
-                         add-filter
-                         remove-filter
-                         get-devices)]
+                         [get-bus
+                          (->m (is-a?/c bus%))]
+                         [add-filter
+                          (->m (or/c string? false/c) (or/c caps? false/c) exact-integer?)]
+                         [remove-filter
+                          (->m exact-integer? boolean?)]
+                         [get-devices
+                          (->m (listof (instanceof/c device%/c)))])]
                        [device%
-                        (class/c
-                         create-element
-                         get-caps
-                         get-device-class
-                         get-display-name
-                         has-classes?)]))
+                        device%/c]))
 
 (define device-monitor-mixin
   (make-gobject-delegate get-bus
@@ -28,7 +30,11 @@
 (define device-monitor%
   (class (device-monitor-mixin gobject%)
     (super-new)
-    (inherit-field pointer)))
+    (inherit-field pointer)
+    (define/override (get-bus)
+      (new bus% [pointer (super get-bus)]))
+    (define/override (get-devices)
+      (map (lambda (device) (new device% [pointer device])) (super get-devices)))))
 
 (define device-mixin
   (make-gobject-delegate create-element
@@ -40,4 +46,20 @@
 (define device%
   (class (device-mixin gobject%)
     (super-new)
-    (inherit-field pointer)))
+    (inherit-field pointer)
+    (define/override (create-element [name #f])
+      (let ([el (super create-element name)])
+        (new element% [pointer el])))))
+
+(define device%/c
+  (class/c
+   [create-element
+    (->*m () ((or/c string? false/c)) (is-a?/c element%))]
+   [get-caps
+    (->m caps?)]
+   [get-device-class
+    (->m string?)]
+   [get-display-name
+    (->m string?)]
+   [has-classes?
+    (->m string? boolean?)]))
