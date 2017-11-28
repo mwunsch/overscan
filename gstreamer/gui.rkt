bdfab2da347e028adb0c4e518f822fc1e9ebb370
Mark Wunsch <mark@markwunsch.com>
2017-11-28 16:04:00 -0500

    I have no idea what I'm doing.

--- gstreamer/gui.rkt
+++ gstreamer/gui.rkt
@@ -1,9 +1,12 @@
 #lang racket/gui
 
+;;; Experimental AF
+
 (require ffi/unsafe
          ffi/unsafe/define
          ffi/unsafe/introspection
          racket/class
+         (only-in racket/function thunk)
          (rename-in racket/contract [-> ->>])
          gstreamer/gst
          gstreamer/element
@@ -21,17 +24,21 @@
                                exact-integer?
                                exact-integer?
                                exact-integer?
-                               boolean?)]
-                         [set-window-handle!
-                          (->m void?)])]
+                               boolean?)])]
                        [make-gui-sink
                         (->> (is-a?/c video-overlay%))]))
 
 (define gst-video
   (introspection 'GstVideo))
 
+(define gst-gl
+  (introspection 'GstGL))
+
 (define video-overlay-interface
   (gst-video 'VideoOverlayInterface))
+
+(define gst-glcontext
+  (gst-gl 'GLContext))
 
 ;;; VideoOverlayInterface is a gi-struct that defines its members as
 ;;; virtual functions, which ffi/unsafe/introspection does not yet
@@ -49,29 +56,35 @@
 (define-gst-video set-render-rectangle (_fun _pointer _int _int _int _int -> _bool)
   #:c-id gst_video_overlay_set_render_rectangle)
 
+(define-gst-video handle-events (_fun _pointer _bool -> _void)
+  #:c-id gst_video_overlay_handle_events)
+
 (define-gst-video prepare-window-handle-msg? (_fun _pointer -> _bool)
   #:c-id gst_is_video_overlay_prepare_window_handle_message)
 
 (define video-overlay%
-  (class element%
+  (class* element% ()
     (super-new)
     (inherit-field pointer)
-    (init-field [context (new frame%
-                              [label "hello, world"]
+    (init-field [label (gobject-send pointer 'get_name)]
+                [window (new frame%
+                              [label label]
                               [width 640]
-                              [height 480])])
+                              [height 480])]
+                [canvas (new canvas%
+                             [parent window]
+                             [style '(gl no-autoclear)])])
     (define/public (expose!)
       (expose pointer))
     (define/public (set-render-rectangle x y width height)
       (set-render-rectangle pointer x y width height))
-    (define/public (set-window-handle!)
-      (let ([handle (send context get-client-handle)])
-        (set-window-handle pointer handle)))))
+    (define/public (get-glcontext)
+      (gobject-get pointer "context" gst-glcontext))
+
+    (set-window-handle pointer (send canvas get-client-handle))))
 
 (define (make-gui-sink)
   (let* ([el (element-factory%-make "glimagesink")]
-         [ptr (get-field pointer el)]
-         [overlay (new video-overlay% [pointer ptr])])
-    (send overlay set-window-handle!)
-    (send (get-field context overlay) show #t)
-    overlay))
+         [ptr (get-field pointer el)])
+    (parameterize ([current-eventspace (make-eventspace)])
+      (new video-overlay% [pointer ptr]))))
