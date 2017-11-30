8e955dbfd681534c86850124928a1f9169e237ff
Mark Wunsch <mark@markwunsch.com>
2017-11-30 10:04:29 -0500

    Move camera/audio/screen sources out into a special macos module

--- overscan/main.rkt
+++ overscan/main.rkt
@@ -9,7 +9,7 @@
 
 (provide (contract-out [broadcast
                         (->* ()
-                             (source? sink?)
+                             ((is-a?/c element%) (is-a?/c element%))
                              (is-a?/c pipeline%))]
                        [start
                         (-> (is-a?/c pipeline%) thread?)]
@@ -37,22 +37,7 @@
                        [graphviz
                         (->* (path-string?)
                              ((is-a?/c pipeline%))
-                             any)]
-                       [audio-sources
-                        (vectorof (is-a?/c device%))]
-                       [audio
-                        (-> exact-nonnegative-integer? source?)]
-                       [camera-sources
-                        (vectorof (-> source?))]
-                       [camera
-                        (-> exact-nonnegative-integer? source?)]
-                       [screen-sources
-                        (vectorof (-> source?))]
-                       [screen
-                        (->* (exact-nonnegative-integer?)
-                             (#:capture-cursor boolean?
-                              #:capture-clicks boolean?)
-                             source?)])
+                             any)])
          (all-from-out racket/base
                        racket/class
                        gstreamer
@@ -124,64 +109,7 @@
 (define (remove-listener key)
   (hash-remove! broadcast-listeners key))
 
-(define audio-sources
-  (let ([monitor (device-monitor%-new)])
-    (if (positive? (send monitor add-filter "Audio/Source" #f))
-        (for/vector ([device (send monitor get-devices)]
-                     [i (in-naturals)])
-          (displayln (format "Audio Device ~a: ~a" i (send device get-display-name)))
-          device)
-        (and (displayln "No Audio Devices detected.")
-             (vector)))))
 
-(define (audio ref)
-  (let ([device (vector-ref audio-sources ref)])
-    (send device create-element)))
-
-;;; TODO: Abstract avfvideosrc specifics out
-(define camera-sources
-  (let* ([factory (element-factory%-find "avfvideosrc")]
-         [test-el (send factory create)])
-    (define (source-exists? ref)
-      (gobject-set! test-el "device-index" ref)
-      (and (eq? 'success (send test-el set-state 'ready))
-           (send test-el set-state 'null)
-           ref))
-    (for/vector ([i (in-naturals)]
-                 #:break (not (source-exists? i)))
-      (displayln (format "Camera ~a: ~a" i (hash-ref (send factory get-metadata)
-                                                     'long-name)))
-      (lambda () (gobject-with-properties (send factory create)
-                                     (hash 'device-index i))))))
-
-(define screen-sources
-  (let* ([factory (element-factory%-find "avfvideosrc")]
-         [test-el (send factory create)])
-    (define (source-exists? ref)
-      (gobject-set! test-el "capture-screen" #t)
-      (gobject-set! test-el "device-index" ref)
-      (and (eq? 'success (send test-el set-state 'ready))
-           (send test-el set-state 'null)
-           ref))
-    (for/vector ([i (in-naturals)]
-                 #:break (not (source-exists? i)))
-      (displayln (format "Screen Capture ~a: ~a" i (hash-ref (send factory get-metadata)
-                                                     'long-name)))
-      (lambda () (gobject-with-properties (send factory create)
-                                     (hash 'capture-screen #t
-                                           'device-index i))))))
-
-(define (camera ref)
-  (let ([device (vector-ref camera-sources ref)])
-    (device)))
-
-(define (screen ref
-                #:capture-cursor [cursor? #f]
-                #:capture-clicks [clicks? #f])
-  (let ([device (vector-ref screen-sources ref)])
-    (gobject-with-properties (device)
-                             (hash 'capture-screen-cursor cursor?
-                                   'capture-screen-mouse-clicks clicks?))))
 
 (define (broadcast [source (videotestsrc #:live? #t)]
                    [sink (element-factory%-make "fakesink")])
