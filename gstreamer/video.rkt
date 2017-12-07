c98f149281787046b129ef2454b0593e399fe640
Mark Wunsch <mark@markwunsch.com>
2017-12-07 14:35:42 -0500

    write wrapper for video-frame-map

--- gstreamer/video.rkt
+++ gstreamer/video.rkt
@@ -1,6 +1,7 @@
 #lang racket/base
 
 (require ffi/unsafe/introspection
+         (only-in ffi/unsafe malloc)
          racket/class
          racket/contract
          gstreamer/gst
@@ -10,6 +11,8 @@
 (provide (contract-out [video-meta?
                         (-> any/c boolean?)]
                        [video-info?
+                        (-> any/c boolean?)]
+                       [video-frame?
                         (-> any/c boolean?)]
                        [buffer-video-meta
                         (-> buffer?
@@ -22,7 +25,12 @@
                         (-> video-meta?
                             (gi-enum-value/c gst-video-format))]
                        [caps->video-info
-                        (-> caps? (or/c video-info? false/c))]))
+                        (-> caps? (or/c video-info? false/c))]
+                       [video-frame-map
+                        (-> video-info?
+                            buffer?
+                            (gi-bitmask-value/c (gst 'MapFlags))
+                            (or/c video-frame? false/c))]))
 
 (define gst-video
   (introspection 'GstVideo))
@@ -36,11 +44,17 @@
 (define gst-video-format
   (gst-video 'VideoFormat))
 
+(define gst-video-frame
+  (gst-video 'VideoFrame))
+
 (define (video-meta? v)
   (is-gtype? v gst-video-meta))
 
 (define (video-info? v)
   (is-gtype? v gst-video-info))
+
+(define (video-frame? v)
+  (is-gtype? v gst-video-frame))
 
 (define buffer-video-meta
   (gst-video 'buffer_get_video_meta))
@@ -56,3 +70,9 @@
   (let ([info (gst-video-info 'new)])
     (and (gst-video-info 'from_caps info caps)
          info)))
+
+(define (video-frame-map info buffer flags)
+  (let* ([_videoframe (_gi-struct gst-video-frame)]
+         [frame (gstruct-cast (malloc _videoframe) gst-video-frame)])
+    (and (gobject-send frame 'map info buffer flags)
+         frame)))
