c327d59cb2845a3696b2107a0e25d58995f69dde
Mark Wunsch <mark@markwunsch.com>
2017-12-08 13:49:15 -0500

    draw gltexture to glcanvas

--- gstreamer/gui.rkt
+++ gstreamer/gui.rkt
@@ -29,8 +29,36 @@
 (define (gl-memory? mem)
   ((gst-gl 'is_gl_memory) mem))
 
-(define gl-memory
+(define gst-glmemory
   (gst-gl 'GLMemory))
+
+(define gst-glcontext
+  (gst-gl 'GLContext))
+
+(define gst-glcontext%
+  (class gst-object%
+    (super-new)
+    (inherit-field pointer)))
+
+(define glcanvas%
+  (class canvas%
+    (inherit refresh with-gl-context swap-gl-buffers)
+    (super-new [style '(gl no-autoclear)])
+
+    (define/public (get-gl-context-handle)
+      (with-gl-context
+        (thunk
+         (send (get-current-gl-context) get-handle))))
+
+    (define/override (on-size width height)
+      (with-gl-context
+        (thunk
+         (glViewport 0 0 width height)
+         (glMatrixMode GL_PROJECTION)
+         (glLoadIdentity)
+         (glOrtho 0 width 0 height -1 1)
+         (glMatrixMode GL_MODELVIEW)))
+      (refresh))))
 
 (define canvas-sink%
   (class appsink%
@@ -38,10 +66,10 @@
     (inherit-field pointer)
     (init-field [label (gobject-send pointer 'get_name)]
                 [window (new frame%
-                              [label label])]
-                [canvas (new canvas%
-                             [parent window]
-                             [style '(gl no-autoclear)])])
+                             [label label])])
+
+    (field [canvas (new glcanvas%
+                        [parent window])])
 
     (define/public (resize-area width height)
       (define-values (client-width client-height)
@@ -54,11 +82,32 @@
           (send window resize width (+ height-delta height)))))
 
     (define (draw-gl-texture memory)
-      (let ([txid (gl-memory 'get_texture_id (first memory))])
+      (let* ([plane (first memory)]
+             [txid (gst-glmemory 'get_texture_id plane)])
         (send canvas with-gl-context
               (thunk
                (glClearColor 0 0 0 0)
-               (glClear GL_COLOR_BUFFER_BIT)))
+               (glClear GL_COLOR_BUFFER_BIT)
+
+               (glMatrixMode GL_PROJECTION)
+               (glLoadIdentity)
+
+               (glEnable GL_TEXTURE_2D)
+               (glBindTexture GL_TEXTURE_2D txid)
+
+               (glBegin GL_QUADS)
+               (glTexCoord2f 0.0 0.0)
+               (glVertex2f -1.0 -1.0)
+
+               (glTexCoord2f 0.0 1.0)
+               (glVertex2f -1.0 1.0)
+
+               (glTexCoord2f 1.0 1.0)
+               (glVertex2f 1.0 1.0)
+
+               (glTexCoord2f 1.0 0.0)
+               (glVertex2f 1.0 -1.0)
+               (glEnd)))
         (send canvas swap-gl-buffers)))
 
     (define/augment (on-sample sample)
@@ -79,7 +128,9 @@
       (send window show #f))))
 
 (define (make-gui-sink [name #f])
-  (let ([sink (make-appsink #f canvas-sink%)])
+  (let* ([sink (make-appsink #f canvas-sink%)]
+         [canvas (get-field canvas sink)]
+         [canvas-handle (send canvas get-gl-context-handle)])
     (bin%-compose name
                   (capsfilter (string->caps "video/x-raw,format=UYVY"))
                   (element-factory%-make "glupload")
