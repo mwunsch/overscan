f2e0161b599c1750aee4f3f58f5437992c578948
Mark Wunsch <mark@markwunsch.com>
2018-02-14 15:36:36 -0500

    new take on picture-in-picture

--- overscan/video.rkt
+++ overscan/video.rkt
@@ -4,10 +4,15 @@
          racket/contract
          gstreamer)
 
-(provide (contract-out [make-video-mixer
+(provide (contract-out [picture-in-picture
                         (->* ()
                              ((or/c string? #f))
-                             (element/c "videomixer"))]))
+                             (is-a?/c bin%))]))
 
-(define (make-video-mixer [name #f])
-  (element-factory%-make "videomixer" name))
+(define (picture-in-picture video1 video2 [name #f])
+  (let ([mixer (videomixer)]
+        [bin (bin%-new name)])
+    (send bin add-many video1 video2 mixer)
+    (send video1 link mixer)
+    (send video2 link mixer)
+    bin))
