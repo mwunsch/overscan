fc61044407de1d09418cbf929d1a6fccf818987b
Mark Wunsch <mark@markwunsch.com>
2018-05-08 07:32:28 -0400

    add use-clock method to pipeline

--- gstreamer/pipeline.rkt
+++ gstreamer/pipeline.rkt
@@ -14,12 +14,16 @@
                           (->m (is-a?/c bus%))]
                          [get-pipeline-clock
                           (->m (is-a?/c clock%))]
+                         [use-clock
+                          (->m (is-a?/c clock%)
+                               void?)]
                          [get-latency
                           (->m clock-time?)])]))
 
 (define pipeline-mixin
   (make-gobject-delegate get-bus
                          get-pipeline-clock
+                         use-clock
                          get-latency))
 
 (define pipeline%
