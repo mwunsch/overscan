cf6c5a82dfd722a892630959d32f679013e053b4
Mark Wunsch <mark@markwunsch.com>
2018-07-06 13:03:18 -0400

    update deps with additional libraries

--- info.rkt
+++ info.rkt
@@ -1,7 +1,10 @@
 #lang info
 (define collection 'multi)
 (define deps '("base"
-               "rackunit-lib"))
+               "rackunit-lib"
+               "draw-lib"
+               "gui-lib"
+               "sgl"))
 (define build-deps '("scribble-lib" "racket-doc"))
 (define pkg-desc "Video graphics companion")
 (define version "0.1")
