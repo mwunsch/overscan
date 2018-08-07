357e1335e3c1a3110bf9ad81b4a377a03491753f
Mark Wunsch <mark@markwunsch.com>
2018-08-07 17:00:14 -0400

    we're going to port this pipeline over to racket/overscan

--- docs/examples/logo.rkt
+++ docs/examples/logo.rkt
@@ -1,1 +1,10 @@
 #lang overscan
+
+;; gst-launch-1.0 videomixer name=final ! videoconvert ! video/x-raw,width=640,height=640 ! osxvideosink sync=false \
+;;                filesrc location=racket-logo.svg.png ! pngdec ! imagefreeze ! videoconvert ! tee name=logo \
+;;                videomixer name=background ! final. \
+;;                videomixer name=foreground ! alpha method=custom target-r=253 target-g=164 target-b=40 ! final. \
+;;                videotestsrc pattern=snow ! video/x-raw,width=640,height=640 ! background. \
+;;                logo. ! queue ! alpha method=custom target-r=253 target-g=164 target-b=40 ! background. \
+;;                videotestsrc pattern=smpte100 ! video/x-raw,width=640,height=640 ! foreground. \
+;;                logo. ! queue ! alpha method=custom target-r=127 target-g=15 target-b=126 ! foreground.
