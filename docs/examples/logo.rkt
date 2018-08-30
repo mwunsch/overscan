7a9bfae361a6b9123093b73f3c3c0bb386c0f9a0
Mark Wunsch <mark@markwunsch.com>
2018-08-30 09:52:59 -0400

    overwrite overscan stop fn

--- docs/examples/logo.rkt
+++ docs/examples/logo.rkt
@@ -1,26 +1,18 @@
 #lang overscan
 
 (require (only-in ffi/unsafe/introspection
-                  gobject-with-properties))
-
-;; gst-launch-1.0 videomixer name=final ! videoconvert ! video/x-raw,width=640,height=640 ! osxvideosink sync=false \
-;;                filesrc location=racket-logo.svg.png ! pngdec ! imagefreeze ! videoconvert ! tee name=logo \
-;;                videomixer name=background ! final. \
-;;                videomixer name=foreground ! alpha method=custom target-r=253 target-g=164 target-b=40 ! final. \
-;;                videotestsrc pattern=snow ! video/x-raw,width=640,height=640 ! background. \
-;;                logo. ! queue ! alpha method=custom target-r=253 target-g=164 target-b=40 ! background. \
-;;                videotestsrc pattern=smpte100 ! video/x-raw,width=640,height=640 ! foreground. \
-;;                logo. ! queue ! alpha method=custom target-r=127 target-g=15 target-b=126 ! foreground.
-
+                  gobject-with-properties)
+         (prefix-in overscan: (only-in overscan
+                                       stop)))
 
 (define pipeline
   (let* ([pl (pipeline%-new)]
          [foreground (videomixer "foreground")]
-         [forealpha (gobject-with-properties (element-factory%-make "alpha")
-                                             (hash 'method 3
-                                                   'target-r 253
-                                                   'target-g 164
-                                                   'target-b 40))]
+         [forea (gobject-with-properties (element-factory%-make "alpha")
+                                         (hash 'method 3
+                                               'target-r 253
+                                               'target-g 164
+                                               'target-b 40))]
          [background (videomixer "background")]
          [logosrc (bin%-compose "logo"
                                 (filesrc (build-path (current-directory) "racket-logo.svg.png"))
@@ -56,7 +48,7 @@
     (and (send pl add-many logosrc logotee)
          (send pl add-many forepattern foreq foreground)
          (send pl add-many backpattern backq background)
-         (send pl add-many forealpha destination sink)
+         (send pl add-many forea destination sink)
          (send logosrc link logotee)
          (send logotee link foreq)
          (send logotee link backq)
@@ -65,8 +57,8 @@
          (send backpattern link-filtered background (video/x-raw 640 640))
          (send backq link background)
          (send background link destination)
-         (send foreground link forealpha)
-         (send forealpha link destination)
+         (send foreground link forea)
+         (send forea link destination)
          (send destination link sink)
          pl)))
 
@@ -75,3 +67,17 @@
 (define (end)
   (let ([encoder (send pipeline get-by-name "encoder")])
     (send encoder send-event (make-eos-event))))
+
+(define (stop)
+  (and (end)
+       (overscan:stop)))
+
+
+;; gst-launch-1.0 videomixer name=final ! videoconvert ! video/x-raw,width=640,height=640 ! osxvideosink sync=false \
+;;                filesrc location=racket-logo.svg.png ! pngdec ! imagefreeze ! videoconvert ! tee name=logo \
+;;                videomixer name=background ! final. \
+;;                videomixer name=foreground ! alpha method=custom target-r=253 target-g=164 target-b=40 ! final. \
+;;                videotestsrc pattern=snow ! video/x-raw,width=640,height=640 ! background. \
+;;                logo. ! queue ! alpha method=custom target-r=253 target-g=164 target-b=40 ! background. \
+;;                videotestsrc pattern=smpte100 ! video/x-raw,width=640,height=640 ! foreground. \
+;;                logo. ! queue ! alpha method=custom target-r=127 target-g=15 target-b=126 ! foreground.
